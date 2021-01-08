c80a6a25d91468c1a1700df8e26dea7da394c5a3
sun912 <gslee100@gmail.com>
2021-01-08 16:22:54 +0000

    BF_N&M(4).py done

--- BF_N&M(4).py
+++ BF_N&M(4).py
@@ -1,10 +1,12 @@
+#15652
+
 n, m = map(int, input().split())
 if n > 8:
   quit()
 a = [0]*m
 # c = [False]*(n+1)
 
-def go(index,start, n, m):
+def go(index, start, n, m):
   if(index==m):
     print(' '.join(map(str,a)))
     return
@@ -12,10 +14,9 @@ def go(index,start, n, m):
   for i in range(start,n+1):
     # if c[i]:
       # continue
-   
     a[index] = i
     # c[i] = True
-    go(index+1,i+1,n,m)
+    go(index+1,i,n,m)
     # c[i] = False
 
 go(0,1,n,m)
