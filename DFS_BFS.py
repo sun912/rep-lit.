f4f8b6a9263bacbeb872456a691cea363b11fde1
sun912 <gslee100@gmail.com>
2021-01-16 15:46:16 +0000

    connected component.py finished

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -1,38 +1,39 @@
 from collections import deque
-v, e, s = map(int,input().split())
-a = [[] for _ in range(e)]
+v,e,start = map(int,input().split())
+a = [[] for _ in range(v+1)]
 c = [False]*(v+1)
+
 for _ in range(e):
-  u,v = map(int, input().split())
+  u,v = map(int, input().split())  
   a[u].append(v)
   a[v].append(u)
 for i in range(v):
   a[i].sort()
 
-def dfs(x):
-  global c
+def DFS(x):
+  global check
   c[x] = True
-  print(x, end=' ')
-  for y in a[x]:
-    if c[y] == False:
-      dfs(y)
-    
-def bfs(x):
-  c = [False]*(v+1)
+  print(x, end = ' ')
+
+  for i in a[x]:
+    if c[i] == False:
+      DFS(a[i])
+
+def BFS(start):
+  check = [False]*(v+1)
   q = deque()
-  q.append(s)
-  c[s] = True
+  q.append(start)
+  check[start] = True
   while q:
     x = q.popleft()
     print(x, end = ' ')
     for y in a[x]:
-      if c[y] == False:
-        c[y] = True
+      if check[y] == False:
+        check[y] = True
         q.append(y)
 
-dfs(s)
+DFS(start)
 print()
-bfs(s)
+BFS(start)
 print()
-
 
